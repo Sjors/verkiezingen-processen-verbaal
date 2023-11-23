702d91730ceb31bb1704a72107a43eee79a18df7
Sjors Provoost <sjors@sprovoost.nl>
2023-11-23 21:20:53 +0100

    Verbeter Python script

--- urls-from-html.py
+++ urls-from-html.py
@@ -3,7 +3,9 @@
 #
 # pip install bs4
 #
-# Usage: ./urls-from-html.py https://www.voorneaanzee.nl/processen-verbaal-tweede-kamerverkiezing-2023
+# Usage:
+# ./urls-from-html.py https://www.voorneaanzee.nl/processen-verbaal-tweede-kamerverkiezing-2023
+# ./urls-from-html.py https://www.venray.nl/uitslag-tweede-kamerverkiezingen-2023 "/file/proces" https://www.venray.nl
 
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
@@ -12,7 +14,10 @@ import sys
 
 response = urlopen(sys.argv[1]).read()
 soup= BeautifulSoup(response, "html.parser")
-links = soup.find_all('a', href=re.compile(r'(\.pdf)'))
+
+regex = sys.argv[2] if len(sys.argv) > 2 else ".pdf"
+prefix = sys.argv[3] if len(sys.argv) > 3 else ""
+links = soup.find_all('a', href=re.compile(r'(' + regex + ')'))
 
 for el in links:
-    print(el['href'])
+    print(prefix + el['href'])
