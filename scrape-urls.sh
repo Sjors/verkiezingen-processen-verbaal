28451d37edf666e0ecfb332b36f7fd1e6f23bb90
Sjors Provoost <sjors@sprovoost.nl>
2023-11-24 21:03:52 +0100

    Rotterdam

--- scrape-urls.sh
+++ scrape-urls.sh
@@ -5,4 +5,5 @@ stad () {
 pushd 2023-TK
     ../urls-from-html.py https://www.utrecht.nl/bestuur-en-organisatie/verkiezingen/ > "$(stad 0344)" ".pdf" https://www.utrecht.nl
     ../urls-from-html.py https://www.eindhoven.nl/bestuur-en-beleid/verkiezing-tweede-kamer-2023/verkiezingsuitslagen > "$(stad 0772)"
+    ../urls-from-html.py https://tellingen.stembureausinrotterdam.nl/Documenten-TK23 > "$(stad 0599)" ".pdf" https://tellingen.stembureausinrotterdam.nl
 popd
