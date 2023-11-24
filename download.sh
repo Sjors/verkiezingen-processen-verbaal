bcb610431eec9a4c1b85163dbd15dbabbe325ebb
Sjors Provoost <sjors@sprovoost.nl>
2023-11-24 10:07:48 +0100

    Gebruik --content-disposition waar nodig
    URL's van het type dsresource?objectid=c52cd... zouden anders allemaal "dsresource" heten. Door --content-disposition krijgen ze de juiste naam.
    
    Nadeel van deze aanpak is dat je een bestand eerst moet downloaden om de naam te weten. Dat is hinderlijk als je het download script meerdere keren gebruikt. Om dat te beperken, wordt alleen (alles opnieuw) gedownload als het aantal bestanden is toegenomen. Bestaande bestanden worden daarbij niet vervangen.
    
    Fixes #8

--- download.sh
+++ download.sh
@@ -6,14 +6,41 @@ pushd 2023-TK
     find *.txt -type f -size +0 | while read -r file; do
         n=`echo $file | awk '{print $1}'`
         naam=`echo $file | awk '{print substr($2, 1, length($2)-4)}'`
-        echo "Gemeente $n: $naam"
         mkdir -p $n
-        cat "$file" | while read url || [[ -n $url ]]; do
-          dest="$n/`basename $url | cut -d? -f1`"
-          if [ ! -s "$dest" ]; then
-            wget $url -O "$dest" --no-check-certificate
-          fi
-        done
+        # Tel aantal reeds gedownloade bestanden:
+        # https://stackoverflow.com/a/11131443/313633
+        shopt -s nullglob
+        n_files=($n/*)
+        n_files=${#n_files[@]}
+        # Tel aantal regels in text bestand:
+        n_urls=`grep -c ^ "$file"`
+        # Sla over als er niet meer files zijn, dit is met name
+        # nodig voor de --content-disposition bestanden, om
+        # te voorkomen dat we die servers hameren.
+        if (( $n_urls > $n_files )); then
+          echo "Gemeente $n: $naam, $n_urls bestanden beschikbaar, $n_files reeds gedownload..."
+          cat "$file" | while read url || [[ -n $url ]]; do
+            case $n in
+              0317|0677|1680)
+                # URL's van het type dsresource?objectid=c52cd...
+                # Deze zouden anders allemaal "dsresource" heten.
+                # --no-clobber zorgt dat bestaande bestanden niet vervangen
+                # worden.
+                # --restrict-file-names is nodig voor bestandsnamen
+                # als "Gemeenschapscentrum ´t Heike"
+                # https://stackoverflow.com/a/22013384/313633
+                wget "$url" --content-disposition --restrict-file-names=ascii --no-check-certificate --no-clobber --directory-prefix=$n
+                ;;
+              *)
+                # Download alleen nieuwe bestanden (negeert wijzigigen)
+                dest="$n/`basename $url | cut -d? -f1`"
+                if [ ! -s "$dest" ]; then
+                  wget $url -O "$dest" --no-check-certificate
+                fi
+                ;;
+            esac
+          done
+        fi
     done
 
 popd
