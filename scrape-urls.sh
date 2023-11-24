27ecef90d5b6dc7f9db5482e2f474161687ad37d
Sjors Provoost <sjors@sprovoost.nl>
2023-11-24 20:58:27 +0100

    Eindhoven de gekste

--- scrape-urls.sh
+++ scrape-urls.sh
@@ -4,4 +4,5 @@ stad () {
 }
 pushd 2023-TK
     ../urls-from-html.py https://www.utrecht.nl/bestuur-en-organisatie/verkiezingen/ > "$(stad 0344)" ".pdf" https://www.utrecht.nl
+    ../urls-from-html.py https://www.eindhoven.nl/bestuur-en-beleid/verkiezing-tweede-kamer-2023/verkiezingsuitslagen > "$(stad 0772)"
 popd
