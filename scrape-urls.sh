6e03db1eeb772043c3a54ae64cb0c7bfab78c16b
Sjors Provoost <sjors@sprovoost.nl>
2023-11-25 16:35:36 +0100

    Oost Gelre

--- scrape-urls.sh
+++ scrape-urls.sh
@@ -71,6 +71,7 @@ pushd 2023-TK
     # N
 
     # O
+    ../urls-from-html.py https://www.oostgelre.nl/uitslagen-tweede-kamerverkiezingen-22-november-2023-oost-gelre ".pdf" https://www.oostgelre.nl > "$(stad 1586)"
     ../urls-from-html.py https://oostzaan.nl/verkiezingen/uitslagen-verkiezingen-tweede-kamer-2023 ".pdf" https://oostzaan.nl > "$(stad 0431)"
     ../urls-from-html.py https://www.opsterland.nl/verkiezingen/uitslagen | grep -v docreader > "$(stad 0086)"
     ../urls-from-html.py https://www.oss.nl/Tonen-op-pagina-standaard/Uitslag-Tweede-Kamer-verkiezingen-2.htm "to2" https://www.oss.nl > "$(stad 0828)"
