5dda41800c28c447c0eecb8259cdfa427c05835b
Sjors Provoost <sjors@sprovoost.nl>
2023-11-25 19:39:32 +0100

    Leiderdorp

--- scrape-urls.sh
+++ scrape-urls.sh
@@ -84,6 +84,7 @@ pushd 2023-TK
     # K
 
     # L
+    ../urls-from-html.py https://www.leiderdorp.nl/verkiezingen ".pdf" https://www.leiderdorp.nl > "$(stad 0547)"
     ../urls-from-html.py https://www.lv.nl/voorlopige-uitslag-tweede-kamerverkiezing "/file" https://www.lv.nl > "$(stad 1916)"
     ../urls-from-html.py https://www.lelystad.nl/4/verkiezingen/verkiezingsuitslagen/Tweede-Kamerverkiezing-2023.html ".pdf" https://www.lelystad.nl > "$(stad 0995)"
     ../urls-from-html.py https://www.leudal.nl/uitslag-tweede-kamerverkiezing-2023 > "$(stad 1640)"
