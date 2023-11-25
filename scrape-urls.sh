9e51fe6a012523b320ab63271cc7892c4fd9f070
Sjors Provoost <sjors@sprovoost.nl>
2023-11-25 13:43:21 +0100

    Rijswijk

--- scrape-urls.sh
+++ scrape-urls.sh
@@ -49,6 +49,7 @@ pushd 2023-TK
     # P
 
     # R
+    ../urls-from-html.py https://www.rijswijk.nl/voorlopige-uitslag-tweede-kamerverkiezing-2023 > "$(stad 0603)"
     ../urls-from-html.py https://www.roerdalen.nl/uitslagen-verkiezingen > "$(stad 1669)"
     ../urls-from-html.py https://roosendaal.nl/uitslag-tweede-kamerverkiezing "/download" https://roosendaal.nl > "$(stad 1674)"
     ../urls-from-html.py https://tellingen.stembureausinrotterdam.nl/Documenten-TK23 > "$(stad 0599)" ".pdf" https://tellingen.stembureausinrotterdam.nl
