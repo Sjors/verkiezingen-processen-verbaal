38bd52bb3c106fb90bf5744a3d3963a8de9f98bf
Sjors Provoost <sjors@sprovoost.nl>
2023-11-25 18:13:16 +0100

    Neder-Betuwe

--- scrape-urls.sh
+++ scrape-urls.sh
@@ -81,6 +81,7 @@ pushd 2023-TK
     # M
 
     # N
+    ../urls-from-html.py https://www.nederbetuwe.nl/verkiezingen "dumpFile" > "$(stad 1740)"
     ../urls-from-html.py https://www.nieuwegein.nl/gemeente-bestuur-en-organisatie/verkiezingen ".pdf" https://www.nieuwegein.nl | grep -v docreader > "$(stad 0356)"
     ../urls-from-html.py https://www.nieuwkoop.nl/verkiezingsuitslagen | grep -v docreader > "$(stad 0569)"
     ../urls-from-html.py https://www.nijkerk.eu/uitslag-verkiezingen-tweede-kamer-22-november-2023 > "$(stad 0267)"
