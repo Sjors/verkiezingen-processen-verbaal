c49931fbf3668d9b2c69eaa301aac7d2be6c4fa3
Sjors Provoost <sjors@sprovoost.nl>
2023-11-25 20:24:22 +0100

    Landsmeer

--- scrape-urls.sh
+++ scrape-urls.sh
@@ -88,6 +88,7 @@ pushd 2023-TK
     # K
 
     # L
+    ../urls-from-html.py https://www.landsmeer.nl/nieuws_en_bekendmakingen/tweede_kamer_verkiezingen_2023 bestand https://www.landsmeer.nl > "$(stad 0415)"
     ../urls-from-html.py https://www.lansingerland.nl/politiek-en-organisatie/tweede-kamerverkiezing/processen-verbaal-tweede-kamerverkiezing-2023/ > "$(stad 1621)"
     ../urls-from-html.py https://www.laren.nl/Bestuur/Tweede_Kamerverkiezing_op_22_november_2023 dsresource https://www.laren.nl > "$(stad 0417)"
     ../urls-from-html.py https://gemeente.leiden.nl/bestuur/tweede-kamerverkiezingen-op-22-november-2023/ ".pdf" https://gemeente.leiden.nl > "$(stad 0546)"
