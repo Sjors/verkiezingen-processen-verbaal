880660ec57b61973ad2708320cbfe3e5f0c62707
Sjors Provoost <sjors@sprovoost.nl>
2023-11-25 11:34:46 +0100

    Tubbergen
    Door een foutje in de HTML verwijst de S van "Stembureau 5" naar de download van een ander bureau, waardoor die link dubbelop is. Omdat ik toch aan het debuggen was heb ik wat niet-PV documenten uitgesloten van download.

--- scrape-urls.sh
+++ scrape-urls.sh
@@ -56,6 +56,7 @@ pushd 2023-TK
     ../urls-from-html.py https://tellingen.stembureausindenhaag.nl/Algemeen ".pdf" https://tellingen.stembureausindenhaag.nl >> "$(stad 0518)"
 
     # T
+    ../urls-from-html.py https://www.tubbergen.nl/tweede-kamerverkiezingen-2023 "download" https://www.tubbergen.nl | grep -v 4544 | grep -v 4556 | grep -v 4557 | grep -v 4611 | grep -v 4612  | grep -v 4613 | sort | uniq  > "$(stad 0183)"
     ../urls-from-html.py https://www.twenterand.nl/verkiezingsuitslagen-2023-stembureau ".pdf" > "$(stad 1700)"
     ../urls-from-html.py https://www.tynaarlo.nl/verkiezingen/uitslagen-verkiezingen ".pdf" https://www.tynaarlo.nl > "$(stad 1730)"
     ../urls-from-html.py https://www.t-diel.nl/uitslag-en-inzage-processen-verbaal-van-de-tweede-kamerverkiezing-2023 > "$(stad 0737)"
