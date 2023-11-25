2f212281875122629e8d79a5bf4b0f5b8437594b
roelant <roelantcornelissen@gmail.com>
2023-11-25 17:10:53 +0100

    Bergen (L) + fix mixup (NH)

--- scrape-urls.sh
+++ scrape-urls.sh
@@ -51,9 +51,10 @@ pushd 2023-TK
     echo "https://www.beuningen.nl/projecten/Tweede_Kamerverkiezingen_2023/Uitslag_verkiezing/0209_Proces_verbaal_GSB_TK23.pdf" >> "$(stad 0209)"
     ../urls-from-html.py https://www.beverwijk.nl/verkiezingen > "$(stad 0375)"
     ../urls-from-html.py https://www.bunschoten.nl/uitslag-verkiezingen > "$(stad 0313)"
-    ../urls-from-html.py https://www.bergen-nh.nl/verkiezingen ".pdf" https://www.bergen-nh.nl > "$(stad 0893)"
+    ../urls-from-html.py https://www.bergen-nh.nl/verkiezingen ".pdf" https://www.bergen-nh.nl > "$(stad 0373)"
     ../urls-from-html.py https://www.gemeenteberkelland.nl/verkiezingen/uitslagen/ > "$(stad 1859)"
     ../urls-from-html.py https://www.beekdaelen.nl/uitslagen-tweede-kamer-verkiezingen-22-november-2023 > "$(stad 1954)"
+    ../urls-from-html.py https://www.bergen.nl/tweede-kamer-2023 > "$(stad 0893)"
 
     # C
     ../urls-from-html.py https://www.coevorden.nl/verkiezingsuitslagen "pdf" https://www.coevorden.nl > "$(stad 0109)"
