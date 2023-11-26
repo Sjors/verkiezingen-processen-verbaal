0da4d7c79218c2c5065f27250da208cf9c4e3ac2
Sjors Provoost <sjors@sprovoost.nl>
2023-11-26 12:38:31 +0100

    Kaag en Braassem

--- scrape-urls.sh
+++ scrape-urls.sh
@@ -139,6 +139,7 @@ pushd 2023-TK
 
     # K
     ../urls-from-html.py  https://www.kampen.nl/tweede-kamerverkiezingen ".pdf" https://www.kampen.nl > "$(stad 0166)"
+    ../urls-from-html.py https://www.kaagenbraassem.nl/Bestuur_en_organisatie/Verkiezingen/Definitieve_uitslag_Tweede_Kamerverkiezingen dsresource https://www.kaagenbraassem.nl | grep -v pinterest > "$(stad 1884)"
     # TODO: Kapelle
     ../urls-from-html.py https://www.katwijk.nl/gemeente-en-burgerzaken/verkiezingen/uitslagen ".pdf" https://www.katwijk.nl > "$(stad 0537)"
     ../urls-from-html.py https://www.kerkrade.nl/uitslagen-tweede-kamerverkiezing-2023 > "$(stad 0928)"
